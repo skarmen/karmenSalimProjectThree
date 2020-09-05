0b65a749df797615dc3a9714b0bc41b5952e8248
skarmen <karmennsalim@gmail.com>
2020-09-05 08:44:07 -0400

    initial commit

---
+++ scripts/app.js
@@ -0,0 +1,15 @@
+// Task 1 - create the basic functionality:
+// Users can enter tasks into the input field and press enter to add the task to an unordered list
+// Upon submission, the inputted task will be added to an unordered list below the input field
+// Listen to a submit event on the form
+// Prevent the default behaviour on submit
+// Store the input value in a variable
+// Append that variable to the unordered list
+// Append a list item to the unored list(can use font - awesome icon for checkbox and garbage can - should be append inside the li)
+// Clear the input field once the form is submitted by setting the input field value to ‘’ (empty quotes) once the form has been submitted
+
+// Task 2 - manipulate the tasks in the unordered list:
+// When a user clicks on the list item’s checkbox icon, the checkbox icon will change to clicked and / or the text color will be greyed out or scratched
+// Automatically move completed items to the bottom of the list
+// When a user clicks on the list item’s garbage icon, the item will be removed from the list
+// When a user clicks on the ‘Clear” btn at the bottom of the list the entire content of the list will be removed.
