a13b2c58d8d93f5f53d5b2cfc914ebde68822bb5
skarmen <karmennsalim@gmail.com>
2020-09-07 18:36:11 -0400

    Made list items editabled

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,54 @@
+// MVP Goals
+// Enter task - DONE
+// Delete task - DONE
+// Clear all tasks - DONE
+// Mark task as completed - DONE
+// Automatically move completed items at the bottom of the list - DONE
+
+// Stretch Goals
+// Allow for the creation of multiple lists(e.g Personal, Work, Entertainment)
+// Possibly dropdown menu to select which category you want to open a list for
+// Implement Due date on tasks(alert user / change colors of overdue tasks)
+// Move tasks to and from different lists
+// Edit task(change the task or add more details)
+// Move tasks order in the list(Drag & Drop)
+
+
+// Pseudo Code
 // Task 1 - create the basic functionality:
-// Users can enter tasks into the input field and press add btn to add the task to an unordered list - DONE
-// Upon submission, the inputted task will be added to an unordered list below the input field - DONE
-// - use conditional here to check if the input is valid - DONE
-// Listen to a submit event on the btn - DONE
-// Prevent the default behaviour on submit - DONE
+// Users can enter tasks into the input field and press enter or add btn to add the task to an ordered list - DONE
+// Upon submission, the inputted task will be added to an ordered list below the input field - DONE
+//  - use conditional here to check if the input is valid & handle errors - DONE
+//  - Listen to a submit event on the btn - DONE
+//  - Prevent the default behavior on submit - DONE
 // Store the input value in a variable - DONE
-// Append that variable to the unordered list - DONE
-// Append a list item to the unordered list(can use font - awesome icon for checkbox and garbage can - should be append inside the li) - DONE
+// Append a list item to the ordered list - DONE
+//  - append the user input variable wrapped in a list item - DONE
+//  - include checkbox and remove btn to the list item - DONE
 // Clear the input field once the form is submitted by setting the input field value to ‘’ (empty quotes) once the form has been submitted - DONE
 
-// Task 2 - manipulate the tasks in the unordered list:
-// When a user clicks on the list item’s checkbox icon, the checkbox icon will change to clicked and / or the text color will be greyed out or scratched - DONE
+// Task 2 - manipulate the tasks in the ordered list:
+// When a user clicks on the list item’s checkbox icon, the checkbox icon will change to clicked and the text color (or background-color of the element) will be changed
 // Automatically move completed items to the bottom of the list - DONE
-// When a user clicks on the list item’s garbage icon, the item will be removed from the list
-// When a user clicks on the ‘Clear” btn at the bottom of the list the entire content of the list will be removed.
+// When a user clicks on the list item’s remove btn, the item will be removed from the list - DONE
+// When a user clicks on the ‘Clear” btn at the bottom of the list the entire content of the list will be removed. - DONE
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 $(document).ready(function (event) {
 
@@ -50,18 +85,32 @@ $(document).ready(function (event) {
     // create an icon to remove an element from the list
     // let removeItemEl = '<span id="remove-item" class="remove-item">x(remove item)</span>'
     let removeItem = '<button id="remove">x</button>'
-
-    //
+    
     let checkbox = '<input type="checkbox">'
     // append the added element from the list
-    $('ol').append(`<li>${checkbox} ${userInput} ${removeItem}</li>`);
-
+    $('ol').append(`<li>${checkbox} <span data-id="editable-list-item">${userInput}</span> ${removeItem}</li>`);
     // clear the input field once the item is appended to the list
     $('input').val('')
-
+    configureEditableListItems()
   }
 
+  /* EDIT LIST ITEMS FUNCTION */
+  function configureEditableListItems () {
+    $("#to-do-list li").on('click', 'span[data-id="editable-list-item"]', function () {
+      var $input = $('<input type="text" data-id="editable-list-item">')
+      console.log('this editable:', $(this))
 
+      $input.val($(this).html());
+      $(this).replaceWith($input);
+      $input.focus();
+    });
+
+    $("#to-do-list li").on('focusout', 'input[data-id="editable-list-item"]', function () {
+      var $span = $('<span data-id="editable-list-item">');
+      $span.html($(this).val());
+      $(this).replaceWith($span);
+    });
+  }
 
 
   /* MARK ITEM AS COMPLETED FUNCTION
