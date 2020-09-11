aba6f4c4b86587d90dd35607b28e76a9fe1baee2
skarmen <karmennsalim@gmail.com>
2020-09-11 13:27:03 -0400

    Refactor the code to use namespacing

--- scripts/app.js
+++ scripts/app.js
@@ -50,75 +50,55 @@
 
 
 
-$(document).ready(function (event) {
+const toDoApp = {}
 
-  /* SUBMIT FUNCTION
-    - listen to a click event on submit & prevent the default behaviour of the submit event
-    - validate the userInput and add it to the list by calling (addTask f)
-  */
-  function configureSubmitBehaviour () {
-    $('.add-new-task-btn').on('click', function (e) {
-      e.preventDefault()
+/* SUBMIT FUNCTION
+   - listen to a click event on submit & prevent the default behaviour of the submit event
+   - validate the userInput and add it to the list by calling (addTask f)
+ */
+toDoApp.configureSubmitBehaviour = function () {
+  $('.add-new-task-btn').on('click', function (e) {
+    e.preventDefault()
 
-      const $addBtn = $(e.target)
-      console.log('addBtn:', $addBtn)
+    const $addBtn = $(e.target)
+    console.log('addBtn:', $addBtn)
 
-      const $eventTargetPreviousEl = $(e.target).prev() // e target = btn, so we are looking for the input field before the add task btn
-      console.log('e target:', e.target, 'this:', $(this))
+    const $eventTargetPreviousEl = $(e.target).prev() // e target = btn, so we are looking for the input field before the add task btn
+    console.log('e target:', e.target, 'this:', $(this))
 
-      console.log('evenTargetPreviousEl:', $eventTargetPreviousEl)
+    console.log('evenTargetPreviousEl:', $eventTargetPreviousEl)
 
-
-      // store userInput in a variable
-      const $userInput = $($eventTargetPreviousEl).val().trim() // $.trim has been deprecated in jquery 3.5
-      console.log('userInput:', $userInput)
-
-
-      // check if the input is valid
-      if ($userInput !== '') {
-        addTask($userInput, $addBtn)
-      } else {
-        alert('Input cannot be empty. Please enter a valid task.')
-      }
-    })
-  }
-
-  /* ADD NEW CARD FUNCTION */
-  function configureAddCardBehaviour () {
-    $('#add-new-card-btn').on('click', function (e) {
-      e.preventDefault()
-
-      // append the task card container on btn click
-      addCard()
-      configureSubmitBehaviour()
-
-      // Add editable title on all new cards
-      configureCardTitle()
-    })
-  }
+    // store userInput in a variable
+    const $userInput = $($eventTargetPreviousEl).val().trim() // $.trim has been deprecated in jquery 3.5
+    console.log('userInput:', $userInput)
 
 
-  /* ADD/ EDIT A LIST TITLE FUNCTION
-  */
-  function onCardTitleEdit(result) {
-    console.log(
-      'onEdit()',
-      'this:', this,
-      'result:', result,
-    )
-    return result
-  }
-
-  function configureCardTitle () {
-    $('.editable').editable(onCardTitleEdit, {
-      tooltip: 'Click to edit list title',
-      placeholder: 'Click to edit list title',
-    })
-  }
+    // check if the input is valid
+    if ($userInput !== '') {
+      toDoApp.addTask($userInput, $addBtn)
+    } else {
+      alert('Input cannot be empty. Please enter a valid task.')
+    }
+  })
+}
 
 
-  function addCard() {
-    const $taskCardContainer = $(`
+/* ADD NEW CARD FUNCTION */
+toDoApp.configureAddCardBehaviour = function () {
+  $('#add-new-card-btn').on('click', function (e) {
+    e.preventDefault()
+
+    // append the task card container on btn click
+    toDoApp.addCard()
+    toDoApp.configureSubmitBehaviour()
+
+    // Add editable title on all new cards
+    toDoApp.configureCardTitle()
+  })
+}
+
+toDoApp.addCard = function () {
+  const $taskCardContainer = $(`
      <div class="task-card-container">
         <h2 class="editable"></h2>
         <!-- Input New Task  -->
@@ -130,160 +110,170 @@ $(document).ready(function (event) {
 
 
         <!-- Task List -->
-        <ol id="to-do-list" class="to-do-list sortable">
+        <ol class="to-do-list sortable">
 
           <!-- To do items added dynamically here -->
         </ol>
-        <button id="clear-btn" class="btn clear-list-btn">Clear</button>
+        <button class="btn clear-list-btn">Clear</button>
       </div>
       <!-- Task Board Container ENDS -->
     `)
 
-    $('.main').append($taskCardContainer)
-    console.log('addList works')
-  }
+  $('.main').append($taskCardContainer)
+  console.log('addList works')
+
+}
 
 
-  /* ADD TASK FUNCTION
+/* ADD TASK FUNCTION
     - add the user input to the list
     - clear the input field
     - function is called upon submit
   */
-  function addTask($userInput, $addBtn) {
+toDoApp.addTask = function ($userInput, $addBtn) {
 
-    console.log('addBtn parent next', $($addBtn).parent())
+  console.log('addBtn parent next', $($addBtn).parent())
 
-    // create aa btn to remove an element from the list
-    const removeItem = '<button id="remove">x</button>'
+  // create aa btn to remove an element from the list
+  const removeItem = '<button id="remove">x</button>'
 
-    // create a checkbox to use for checking completed items
-    const checkbox = '<input type="checkbox">'
+  // create a checkbox to use for checking completed items
+  const checkbox = '<input type="checkbox">'
 
-    // append the added element from the list
-    $($addBtn).parent().next('ol').append(`<li>${checkbox} <span data-id="editable-list-item">${$userInput}</span> ${removeItem}</li>`);
-    // clear the input field once the item is appended to the list
-    $('.new-task').val('')
-    configureEditableListItems()
+  // append the added element from the list
+  $($addBtn).parent().next('ol').append(`<li>${checkbox} <span data-id="editable-list-item">${$userInput}</span> ${removeItem}</li>`);
+  // clear the input field once the item is appended to the list
+  $('.new-task').val('')
+  toDoApp.configureEditableListItems()
 
-    // drag & drop - need to look into accessability in order to use it
-    // $(".sortable").sortable()
-
-  }
-
-  // Using jQuery Editable library - alternative to configureEditableListItems
-  // function onEdit(result) {
-  //   console.log(
-  //     'onEdit()',
-  //     'this:', this,
-  //     'result:', result,
-  //   )
-  //   return result
-  // }
-  //$('.editable').editable(onEdit)
+  // drag & drop - need to look into accessability in order to use it
+  // $(".sortable").sortable()
+}
 
 
-  /* EDIT LIST ITEMS FUNCTION
+/* ADD/ EDIT A LIST TITLE FUNCTION
+  */
+toDoApp.onCardTitleEdit = function (result) {
+  console.log(
+    'onEdit()',
+    'this:', this,
+    'result:', result,
+  )
+  return result
+}
+
+toDoApp.configureCardTitle = function () {
+  $('.editable').editable(onCardTitleEdit, {
+    tooltip: 'Click to edit list title',
+    placeholder: 'Click to edit list title',
+  })
+}
+
+
+/* EDIT LIST ITEMS FUNCTION
     - replace the span content with the input value entered by the user
     - replace the span element with the input element
     - reverse on focusout event
   */
-  function configureEditableListItems () {
-    // from https://stackoverflow.com/questions/45985601/how-do-i-make-an-editable-ul-li-list-using-jquery
-    // can be done with editable as well
-    $("#to-do-list li").on('dblclick', 'span[data-id="editable-list-item"]', function () {
-      const $input = $('<input type="text" data-id="editable-list-item">')
+toDoApp.configureEditableListItems = function c() {
+  // from https://stackoverflow.com/questions/45985601/how-do-i-make-an-editable-ul-li-list-using-jquery
+  // can be done with editable as well
+  $(".to-do-list li").on('dblclick', 'span[data-id="editable-list-item"]', function () {
+    const $input = $('<input type="text" data-id="editable-list-item">')
 
-      $input.val($(this).html()) // replace the content of the el
-      console.log('this editable:', $(this)) // this = span
+    $input.val($(this).html()) // replace the content of the el
+    console.log('this editable:', $(this)) // this = span
 
-      $(this).replaceWith($input) // replace the actual el
-      $input.focus()
-    })
+    $(this).replaceWith($input) // replace the actual el
+    $input.focus()
+  })
 
-    $("#to-do-list li").on('keyup focusout', 'input[data-id="editable-list-item"]', function (e) {
-      if(e.keyCode === 13 || e.type ==='focusout') {
+  $(".to-do-list li").on('keyup focusout', 'input[data-id="editable-list-item"]', function (e) {
+    if (e.keyCode === 13 || e.type === 'focusout') {
 
-        const $span = $('<span data-id="editable-list-item">')
+      const $span = $('<span data-id="editable-list-item">')
 
-        $span.html($(this).val())
-        console.log('focusout this:', $(this)) // this = input
+      $span.html($(this).val())
+      console.log('focusout this:', $(this)) // this = input
 
-        $(this).replaceWith($span)
-      }
-    })
-  }
+      $(this).replaceWith($span)
+    }
+  })
+}
 
 
-  /* MARK ITEM AS COMPLETED FUNCTION
+/* MARK ITEM AS COMPLETED FUNCTION
     - mark item as completed & move to the bottom of the list (call moveToBottom)
     - configure the click behaviour
   */
-  function configureMarkItemAsCompleted () {
-    const checkboxSelector = 'input[type=checkbox]'
-    $(document).on('click', checkboxSelector, function (e) {
-      console.log('click')
+toDoApp.configureMarkItemAsCompleted = function () {
+  const checkboxSelector = 'input[type=checkbox]'
+  $(document).on('click', checkboxSelector, function (e) {
+    console.log('click')
 
-      // toggle completed class
-      const $toDoItem = $(this).parent()
-      $($toDoItem).toggleClass('completed')
+    // toggle completed class
+    const $toDoItem = $(this).parent()
+    $($toDoItem).toggleClass('completed')
 
-      if ($(checkboxSelector).is(':checked')) {
-        // store completed item in a variable
-        const $completedItem = $($toDoItem)
+    if ($(checkboxSelector).is(':checked')) {
+      // store completed item in a variable
+      const $completedItem = $($toDoItem)
 
-        const itemCompleted = $completedItem.hasClass('completed') // returns a boolean
+      const itemCompleted = $completedItem.hasClass('completed') // returns a boolean
 
-        if (itemCompleted) {
-          // console.log('item is completed - move to the bottom', completedItem)
-          moveToBottom($completedItem)
-        }
+      if (itemCompleted) {
+        // console.log('item is completed - move to the bottom', completedItem)
+        toDoApp.moveToBottom($completedItem)
       }
-    })
-  }
+    }
+  })
+}
+
+
+// MOVE COMPLETED ITEMS TO THE BOTTOM OF THE LIST
+toDoApp.moveToBottom = function (item) {
+  item.fadeOut(function () {
+    item.appendTo($('ol'))
+    item.fadeIn()
+  })
+}
+
+
+/* REMOVE ITEM FROM THE LIST */
+toDoApp.configureRemoveTask = function () {
+  $(document).on('click', '#remove', function () {
+    const $taskToRemove = $(this).parent() // btn parent -> li
+    // console.log('taskToRemove this:', $(this))
+
+    console.log('taskToRemove:', $taskToRemove)
+    $taskToRemove.remove()
+
+  })
+}
+
+
+/* CLEAR ENTIRE LIST */
+toDoApp.configureClearList = function () {
+  $('.clear-list-btn').on('click', function () {
+    $('ol').empty()
+  })
+}
 
 
 
-  // MOVE COMPLETED ITEMS TO THE BOTTOM OF THE LIST
-  function moveToBottom(item) {
-    item.fadeOut(function () {
-      item.appendTo($('ol'))
-      item.fadeIn()
-    })
-  }
 
-
-  /* REMOVE ITEM FROM THE LIST */
-  function configureRemoveTask () {
-    $(document).on('click', '#remove', function () {
-      const $taskToRemove = $(this).parent() // btn parent -> li
-      // console.log('taskToRemove this:', $(this))
-
-      console.log('taskToRemove:', $taskToRemove)
-      $taskToRemove.remove()
-
-    })
-  }
-
-
-  /* CLEAR ENTIRE LIST */
-  function configureClearList () {
-    $('.clear-list-btn').on('click', function () {
-      $('ol').empty()
-    })
-  }
-
-
+// Code to kick off the app
+toDoApp.init = function () {
   // Function Calls
-  configureAddCardBehaviour ()
-  // configureSubmitBehaviour()
-  configureMarkItemAsCompleted()
-  configureRemoveTask()
-  configureClearList()
+  toDoApp.configureAddCardBehaviour()
+  toDoApp.configureSubmitBehaviour()
+  toDoApp.configureMarkItemAsCompleted()
+  toDoApp.configureRemoveTask()
+  toDoApp.configureClearList()
 
+}
 
-
-
-
-
-  // End of Doc ready function
+// Everything that needs to run on page load
+$(function () {
+  toDoApp.init()
 })
